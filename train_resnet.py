4236cfd210328744dd3dc6e13b4191cd92f1eedc
Ruben Abbou <rabbou@slurm.ttic.edu>
2021-03-15 01:22:32 -0500

    finalized training of resnet

--- train_resnet.py
+++ train_resnet.py
@@ -1,72 +1,105 @@
+import torch
+from torchvision import datasets, transforms as T, models
+import torch.utils.data as tud
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import os
+import scipy.io as sio
 from utils import *
+import argparse
 
-def train():
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--size', type=int, default=64, help='size of patches')
+parser.add_argument('-m', '--magnitude', type=int, default=200, help='magnitude')
+parser.add_argument('-g', '--gpu', type=int, default=2, help='number of GPUs to use')
 
-    net = models.resnet18(pretrained=True)
+args = parser.parse_args()
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
+ext = '_' + str(args.size) + '_' + str(args.magnitude) + 'X.mat'
+train = sio.loadmat('data/train'+ext)
+test = sio.loadmat('data/test'+ext)
+train = DataSet(train['x'].reshape(-1, args.size, args.size, 3), train['y'].flatten())
+test = DataSet(test['x'].reshape(-1, args.size, args.size, 3), test['y'].flatten())
 
-    num_ftrs = net.fc.in_features
-    net.fc = nn.Linear(num_ftrs, 2)
-    net.fc = net.fc
-    net = nn.DataParallel(net).to(device)
+transform = T.Compose([
+        T.ToTensor(),
+        T.Resize(224),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
-    n_epochs = 35
-    print_every = 10
-    valid_loss_min = np.Inf
-    val_loss = []
-    val_acc = []
-    train_loss = []
-    train_acc = []
-    total_step = len(train_loader)
-    for epoch in range(1, n_epochs+1):
-        running_loss = 0.0
-        correct = 0
-        total = 0
-        print(f'Epoch {epoch},', 'lr: %.0e\n' % optimizer.param_groups[0]['lr'])
-        for batch_idx, (data_, target_) in enumerate(train_loader):
-            data_, target_ = data_.to(device, dtype=torch.float), target_.to(device, dtype=torch.long)
-            optimizer.zero_grad()
+train_loader = tud.DataLoader(breakHis(train, transform=transform),
+                              batch_size=1024, shuffle=True, num_workers=8)
+test_loader = tud.DataLoader(breakHis(test, transform=transform),
+                             batch_size=512, shuffle=True, num_workers=8)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-            outputs = net(data_)
-            loss = criterion(outputs, target_)
-            loss.backward()
-            optimizer.step()
+net = models.resnet18(pretrained=True)
 
-            running_loss += loss.item()
-            _,pred = torch.max(outputs, dim=1)
-            correct += torch.sum(pred==target_).item()
-            total += target_.size(0)
-            if (batch_idx) % 20 == 0:
-                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                       .format(epoch, n_epochs, batch_idx, total_step, loss.item()))
-        train_acc.append(100 * correct / total)
-        train_loss.append(running_loss/total_step)
-        print(f'\ntrain-loss: {np.mean(train_loss):.4f}, train-acc: {(100 * correct/total):.4f}')
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3)
 
-        batch_loss = 0
-        total_t=0
-        correct_t=0
-        with torch.no_grad():
-            net.eval()
-            for data_t, target_t in (test_loader):
-                data_t, target_t = data_t.to(device, dtype=torch.float), target_t.to(device, dtype=torch.long)
-                outputs_t = net(data_t)
-                loss_t = criterion(outputs_t, target_t)
-                batch_loss += loss_t.item()
-                _,pred_t = torch.max(outputs_t, dim=1)
-                correct_t += torch.sum(pred_t==target_t).item()
-                total_t += target_t.size(0)
-            val_acc.append(100 * correct_t/total_t)
-            val_loss.append(batch_loss/len(test_loader))
-            network_learned = batch_loss < valid_loss_min
-            print(f'validation loss: {np.mean(val_loss):.4f}, validation acc: {(100 * correct_t/total_t):.4f}\n')
+num_ftrs = net.fc.in_features
+net.fc = nn.Linear(num_ftrs, 2)
+net.fc = net.fc
+net = nn.DataParallel(net).to(device)
 
-            if network_learned:
-                valid_loss_min = batch_loss
-                torch.save(net.state_dict(), 'resnet18.pt')
-                print('Improvement-Detected, save-model')
-        scheduler.step(val_loss[-1])
-        net.train()
+n_epochs = 35
+print_every = 10
+valid_loss_min = np.Inf
+val_loss = []
+val_acc = []
+train_loss = []
+train_acc = []
+total_step = len(train_loader)
+for epoch in range(1, n_epochs+1):
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    print(f'Epoch {epoch},', 'lr: %.0e\n' % optimizer.param_groups[0]['lr'])
+    for batch_idx, (data_, target_) in enumerate(train_loader):
+        data_, target_ = data_.to(device, dtype=torch.float), target_.to(device, dtype=torch.long)
+        optimizer.zero_grad()
+
+        outputs = net(data_)
+        loss = criterion(outputs, target_)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _,pred = torch.max(outputs, dim=1)
+        correct += torch.sum(pred==target_).item()
+        total += target_.size(0)
+        if (batch_idx) % 20 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch, n_epochs, batch_idx, total_step, loss.item()))
+    train_acc.append(100 * correct / total)
+    train_loss.append(running_loss/total_step)
+    print(f'\ntrain-loss: {np.mean(train_loss):.4f}, train-acc: {(100 * correct/total):.4f}')
+
+    batch_loss = 0
+    total_t=0
+    correct_t=0
+    with torch.no_grad():
+        net.eval()
+        for data_t, target_t in (test_loader):
+            data_t, target_t = data_t.to(device, dtype=torch.float), target_t.to(device, dtype=torch.long)
+            outputs_t = net(data_t)
+            loss_t = criterion(outputs_t, target_t)
+            batch_loss += loss_t.item()
+            _,pred_t = torch.max(outputs_t, dim=1)
+            correct_t += torch.sum(pred_t==target_t).item()
+            total_t += target_t.size(0)
+        val_acc.append(100 * correct_t/total_t)
+        val_loss.append(batch_loss/len(test_loader))
+        network_learned = batch_loss < valid_loss_min
+        print(f'validation loss: {np.mean(val_loss):.4f}, validation acc: {(100 * correct_t/total_t):.4f}\n')
+
+        if network_learned:
+            valid_loss_min = batch_loss
+            torch.save(net.state_dict(), 'checkpoints_resnet/resnet18.pt')
+            print('Improvement-Detected, save-model')
+    scheduler.step(val_loss[-1])
+    net.train()
--- utils.py
+++ utils.py
@@ -1,5 +1,44 @@
 import torch
+import torch.utils.data as tud
 
 def accuracy(out, labels):
     _,pred = torch.max(out, dim=1)
     return torch.sum(pred==labels).item()
+
+class DataSet(object):
+    """
+    Class to manage your data
+    If you need to use batch for your algorithm, there is a next_batch implementation
+    """
+
+    def __init__(self, images, classes):
+
+        assert images.shape[0] == classes.shape[0], ('images.shape: %s labels.shape: %s' % (images.shape,classes.shape))
+        self._num_examples = images.shape[0]
+        self._images = images
+        self._classes = classes
+        self._index_in_epoch = 0
+    
+    @property
+    def images(self):
+        return self._images
+    
+    @property
+    def classes(self):
+        return self._classes
+    
+class breakHis(tud.Dataset):
+    def __init__(self, data, transform=None):
+        self.X = data.images
+        self.y = data.classes
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        x = self.X[idx]
+        y = self.y[idx]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
