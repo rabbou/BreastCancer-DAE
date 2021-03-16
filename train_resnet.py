63d7a4263f4db110a3327f2519f02112d7612a2f
Ruben Abbou <ruben@abbou.com>
2021-03-16 12:08:14 -0500

    first sets of results

--- train_resnet.py
+++ train_resnet.py
@@ -11,19 +11,29 @@ from utils import *
 import argparse
 from numpy import array, savetxt
 
+# available_gpu = np.array(list(map(int, os.environ["CUDA_VISIBLE_DEVICES"].split(','))))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--size', type=int, default=64, help='size of patches')
 parser.add_argument('-m', '--magnitude', type=int, default=200, help='magnitude of breakHis dataset to use')
 parser.add_argument('-bs', '--batch-size', type=int, default=512)
 parser.add_argument('-e', '--epochs', type=int, default=35)
+parser.add_argument('-g', '--gpus', nargs='+', type=str, default=os.environ["CUDA_VISIBLE_DEVICES"].split(sep=','),
+                    help='number of GPUs to use')
 
 args = parser.parse_args()
+# gpu_names = available_gpu[args.gpus]
+os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(args.gpus)
+print('Using GPUs:', os.environ["CUDA_VISIBLE_DEVICES"])
 
-ext = '_' + str(args.size) + '_' + str(args.magnitude) + 'X.mat'
-train = sio.loadmat('data/train'+ext)
-test = sio.loadmat('data/test'+ext)
-train = DataSet(train['x'].reshape(-1, args.size, args.size, 3), train['y'].flatten())
-test = DataSet(test['x'].reshape(-1, args.size, args.size, 3), test['y'].flatten())
+ext = str(args.size) + '_' + str(args.magnitude) + 'X.npy'
+with open('data/'+ext, 'rb') as f:
+    train = np.load(f)
+    valid = np.load(f)
+    test = np.load(f)
+train = DataSet(train[:,:-1].reshape(-1, args.size, args.size, 3).astype(float), train[:,-1].astype(np.long))
+valid = DataSet(valid[:,:-1].reshape(-1, args.size, args.size, 3).astype(float), valid[:,-1].astype(np.long))
+test = DataSet(test[:,:-1].reshape(-1, args.size, args.size, 3).astype(float), test[:,-1].astype(np.long))
 
 transform = T.Compose([
         T.ToTensor(),
@@ -53,16 +63,21 @@ print_every = 10
 valid_loss_min = np.Inf
 val_loss = []
 val_acc = []
+val_f1 = []
 train_loss = []
 train_acc = []
+train_f1 = []
 total_step = len(train_loader)
 for epoch in range(1, n_epochs+1):
     running_loss = 0.0
     correct = 0
     total = 0
-    print(f'Epoch {epoch},', 'lr: %.0e\n' % optimizer.param_groups[0]['lr'])
+    TP = 0
+    FP = 0
+    FN = 0
+    print(f'\nEpoch {epoch},', 'lr: %.0e' % optimizer.param_groups[0]['lr'])
     for batch_idx, (data_, target_) in enumerate(train_loader):
-        data_, target_ = data_.to(device, dtype=torch.float), target_.to(device, dtype=torch.long)
+        data_, target_ = data_.to(device, dtype=torch.float), target_.to(device)
         optimizer.zero_grad()
 
         outputs = net(data_)
@@ -74,30 +89,49 @@ for epoch in range(1, n_epochs+1):
         _,pred = torch.max(outputs, dim=1)
         correct += torch.sum(pred==target_).item()
         total += target_.size(0)
+        pos_ind = pred == 1
+        TP += torch.sum(pred[pos_ind]==target_[pos_ind]).item()
+        FP += torch.sum(pred[pos_ind]!=target_[pos_ind]).item()
+        FN += torch.sum(pred[~pos_ind]!=target_[~pos_ind]).item()
         if (batch_idx) % 20 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
                    .format(epoch, n_epochs, batch_idx, total_step, loss.item()))
     train_acc.append(100 * correct / total)
     train_loss.append(running_loss/total_step)
-    print(f'\ntrain-loss: {np.mean(train_loss):.4f}, train-acc: {(100 * correct/total):.4f}')
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    train_f1.append(2/(1/precision + 1/recall))
+    print(f'\ntrain loss: {np.mean(train_loss):.4f}, train acc: {(100 * correct/total):.4f}',
+         f'train f1: {train_f1[-1]:.4f}')
 
     batch_loss = 0
     total_t=0
     correct_t=0
+    TP = 0
+    FP = 0
+    FN = 0
     with torch.no_grad():
         net.eval()
         for data_t, target_t in (test_loader):
-            data_t, target_t = data_t.to(device, dtype=torch.float), target_t.to(device, dtype=torch.long)
+            data_t, target_t = data_t.to(device, dtype=torch.float), target_t.to(device)
             outputs_t = net(data_t)
             loss_t = criterion(outputs_t, target_t)
             batch_loss += loss_t.item()
             _,pred_t = torch.max(outputs_t, dim=1)
             correct_t += torch.sum(pred_t==target_t).item()
             total_t += target_t.size(0)
+            pos_ind = pred == 1
+            TP += torch.sum(pred[pos_ind]==target_[pos_ind]).item()
+            FP += torch.sum(pred[pos_ind]!=target_[pos_ind]).item()
+            FN += torch.sum(pred[~pos_ind]!=target_[~pos_ind]).item()
         val_acc.append(100 * correct_t/total_t)
         val_loss.append(batch_loss/len(test_loader))
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        val_f1.append(2/(1/precision + 1/recall))
         network_learned = batch_loss < valid_loss_min
-        print(f'validation loss: {np.mean(val_loss):.4f}, validation acc: {(100 * correct_t/total_t):.4f}\n')
+        print(f'val loss: {np.mean(val_loss):.4f}, val acc: {(100 * correct_t/total_t):.4f}',
+             f'val f1: {val_f1[-1]:.4f}\n')
 
         if network_learned:
             valid_loss_min = batch_loss
@@ -107,4 +141,4 @@ for epoch in range(1, n_epochs+1):
     net.train()
     
 savetxt('results/' + str(args.size) + '_' + str(args.magnitude) + '_acc.csv',
-        array([train_acc, val_acc]), delimiter=',')
+        array([train_acc, val_acc, train_f1, val_f1]), delimiter=',')
